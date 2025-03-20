c96abd8dddd331b125770dfff761f7545ad95929
abdoudiall <abdoudiall@gmail.com>
2025-03-20 23:16:32 +0100

    fix(terraform): update route53 and s3 configurations

--- terraform/modules/route53/main.tf
+++ terraform/modules/route53/main.tf
@@ -12,7 +12,7 @@ resource "aws_route53_record" "website" {
 
   alias {
     name                   = var.cloudfront_distribution_domain_name
-    zone_id               = var.cloudfront_distribution_hosted_zone_id
+    zone_id                = var.cloudfront_distribution_hosted_zone_id
     evaluate_target_health = false
   }
 }
@@ -25,7 +25,7 @@ resource "aws_route53_record" "www" {
 
   alias {
     name                   = var.cloudfront_distribution_domain_name
-    zone_id               = var.cloudfront_distribution_hosted_zone_id
+    zone_id                = var.cloudfront_distribution_hosted_zone_id
     evaluate_target_health = false
   }
 } 
--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -39,13 +39,13 @@ resource "aws_s3_bucket_policy" "website" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid       = "AllowCloudFrontServicePrincipal"
-        Effect    = "Allow"
+        Sid    = "AllowCloudFrontServicePrincipal"
+        Effect = "Allow"
         Principal = {
           Service = "cloudfront.amazonaws.com"
         }
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.website.arn}/*"
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.website.arn}/*"
         Condition = {
           StringEquals = {
             "AWS:SourceArn" = var.cloudfront_distribution_arn
